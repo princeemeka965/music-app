1bd8e03c677831c0fd7bd062aec3d9ce742c5e55
princeemeka965 <anyanwue4@gmail.com>
2023-10-25 16:25:23 +0100

    first commit

---
+++ src/app/musicStore/index.tsx
@@ -0,0 +1,16 @@
+export const ArtisteStore = (): any => {
+  return [
+    {
+      id: "8292430",
+      name: "Ada Ehi",
+    },
+    {
+      id: "8791550",
+      name: "Adekunle Gold",
+    },
+    {
+      id: "9729060",
+      name: "2Baba",
+    },
+  ];
+};
